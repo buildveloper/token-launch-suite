bdc70b31686589b3e22adc5e31b9b70d73be7384
buildveloper <christopherlegacy01@gmail.com>
2025-04-13 13:41:42 +0100

    Added first solidity code

---
+++ contracts/Token.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+// Importing OpenZeppelin Contracts
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
